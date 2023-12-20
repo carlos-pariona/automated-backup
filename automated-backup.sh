8aaeeb360a58c2bb73a13e5af1d48a6603725380
Carlos Pariona <carlos.parionafa@gmail.com>
2023-12-20 00:07:02 -0500

    Add the automated-backup.sh

---
+++ automated-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This is an example of a Bash automation script using cron jobs.
+# This script will run every day at 8:00 AM and perform a backup of a specific directory.
+
+# Directory path to backup
+DIRECTORY="/path/to/directory"
+
+# Destination path for the backup
+DESTINATION="/path/to/destination"
+
+# Remote server path
+SERVER="user@server:/remote/path"
+
+# Backup file name
+DATE=$(date +"%Y%m%d")
+BACKUP_NAME="backup_$DATE.tar.gz"
+
+# Create the backup
+tar -czvf "$DESTINATION/$BACKUP_NAME" "$DIRECTORY"
+
+# Check if the backup was created successfully
+if [ $? -eq 0 ]; then
+  echo "Backup created successfully."
+  
+  # Send the backup to the remote server using rsync
+  rsync -avz "$DESTINATION/$BACKUP_NAME" "$SERVER"
+  
+  # Check if the transfer was successful
+  if [ $? -eq 0 ]; then
+    echo "Backup sent to the remote server successfully."
+  else
+    echo "Error sending the backup to the remote server."
+  fi
+else
+  echo "Error creating the backup."
+fi
